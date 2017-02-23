0f88480ea215b9796f00ca94bf0fb18025a14ba8
bluecreature <bluecreature@hidden-address>
2017-02-23 15:44:49 +0200

    The IFcModFinder interface
    Find all mods and parse their metadata.

---
+++ FactorioBrowser/FcVersion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FactorioBrowser {
+
+   public class VersionFormatException : Exception {
+      public VersionFormatException() {
+      }
+
+      public VersionFormatException(string message)
+         : base(message) {
+      }
+
+      public VersionFormatException(string message, Exception inner)
+         : base(message, inner) {
+      }
+   }
+
+
+   public sealed class FcVersion {
+      public int Major { get; }
+      public int Minor { get; }
+      public int Patch { get; }
+
+      public FcVersion(int major, int minor, int patch) {
+         Major = major;
+         Minor = minor;
+         Patch = patch;
+      }
+
+      public string ToDotNotation() {
+         return $"{Major}.{Minor}.{Patch}";
+      }
+
+      public override string ToString() {
+         return "FcVersion(" + ToDotNotation() + ")";
+      }
+
+      public static FcVersion FromDotNotation(string versionString) {
+         Match match = DotNotationRegEx.Match(versionString);
+         if (!match.Success) {
+            throw new VersionFormatException($"FcVersion `{versionString}' does not conform " +
+                                             "to the required format <major>.<minor>.<patch>.");
+         }
+
+         int major = Int32.Parse(match.Groups[1].Value);
+         int minor = Int32.Parse(match.Groups[2].Value);
+         int patch = Int32.Parse(match.Groups[3].Value);
+         return new FcVersion(major, minor, patch);
+      }
+
+      private static readonly Regex DotNotationRegEx = new Regex(
+         "^(\\d+).(\\d+).(\\d+)$", RegexOptions.Compiled);
+   }
+}
---
+++ FactorioBrowser/ModFinder/IFcModFinder.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace FactorioBrowser.ModFinder {
+
+   public class FcModInfoException : Exception {
+      public FcModInfoException() {
+      }
+
+      public FcModInfoException(string message)
+         : base(message) {
+      }
+
+      public FcModInfoException(string message, Exception inner)
+         : base(message, inner) {
+      }
+   }
+
+   public enum FcModDeploymentType {
+      Directory,
+      ZipFile,
+   }
+
+   public enum FcVersionRequirement {
+      After,
+      AtLeast,
+      Exactly,
+   }
+
+   public sealed class FcModDependency {
+      public string ModName { get; }
+      public FcVersion Version { get; }
+      public FcVersionRequirement VersionRequirement { get; }
+      public bool Optional { get; }
+
+      public FcModDependency(string modName, FcVersion version,
+         FcVersionRequirement versionRequirement, bool optional) {
+
+         this.ModName = modName;
+         this.Version = version;
+         this.VersionRequirement = versionRequirement;
+         Optional = optional;
+      }
+   }
+
+   public sealed class FcModMetaInfo {
+      public string Path { get; }
+      public FcModDeploymentType DeploymentType { get; }
+      public string Name { get; }
+      public FcVersion Version { get; }
+      public IImmutableList<FcModDependency> Dependencies { get; }
+
+      public FcModMetaInfo(string path, FcModDeploymentType deploymentType, string name,
+         FcVersion version, FcModDependency[] dependencies) {
+
+         Path = path;
+         DeploymentType = deploymentType;
+         Name = name;
+         Version = version;
+
+         if (dependencies != null) {
+            var builder = ImmutableArray.CreateBuilder<FcModDependency>(dependencies.Length);
+            builder.AddRange(dependencies);
+            Dependencies = builder.ToImmutable();
+
+         } else {
+            Dependencies = ImmutableArray.Create<FcModDependency>();
+         }
+      }
+
+      public string VersionedName() {
+         return $"{Name}_${Version.ToDotNotation()}";
+      }
+   }
+
+   public interface IFcModFinder {
+      IImmutableList<FcModMetaInfo> FindAll();
+   }
+}
